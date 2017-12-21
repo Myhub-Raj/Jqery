fabc48c4fddee6cdeb5c7efb84b11b853cf1f38e
Myhub-Raj <rajsimplyfying@gmail.com.com>
2017-12-21 18:19:11 +0530

    second commit

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+console.log("Hello Raj");
+var x = 5;         // assign the value 5 to x
+var y = 3;         // assign the value 2 to y
+var z = x + y;
+console.log(z);
+myHeading.textContent = z;
